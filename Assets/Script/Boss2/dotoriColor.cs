297efcdf41b427893500bd8895e46d5ac081198b
LKJUnity3 <dlrhkdwo2422@gmail.com>
2024-02-28 20:03:18 +0900

    ADD - Bomb

--- Assets/Script/Boss2/dotoriColor.cs
+++ Assets/Script/Boss2/dotoriColor.cs
@@ -9,12 +9,14 @@ public class dotoriColor : MonoBehaviour
     [SerializeField]
     private Transform Boss;
     private Vector3 BossTrans;
+    private Vector3 TargetTrans;
     private float time;
     private void Start()
     {
         BossTrans = Boss.position;
-        target.position = new Vector3(target.position.x, 0, target.position.z);
-        gameObject.GetComponentInChildren<Renderer>().material.color = Color.white;
+        TargetTrans = target.position;
+        TargetTrans = new Vector3(target.position.x, 0, target.position.z);
+        gameObject.GetComponentInChildren<Renderer>().material.color = Color.black;
     }
     
     private void Setup(Transform player)
@@ -29,8 +31,12 @@ public class dotoriColor : MonoBehaviour
         if ( Boss != null)
         {
             time += Time.deltaTime / 2;
-            Vector3 v = Vector3.Lerp(BossTrans, target.position, time);
+            Vector3 v = Vector3.Lerp(BossTrans, TargetTrans, time);
             transform.position = v;
+            if (transform.position == TargetTrans)
+            {
+                Boss = null;
+            }
         }
     }
 
