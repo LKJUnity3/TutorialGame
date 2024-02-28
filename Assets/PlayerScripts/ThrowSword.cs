fa88b528d88a5979ae000c62d287fca796ebb7e3
only1hay <only1hay@nate.com>
2024-02-28 15:50:56 +0900

    [Add]Shoot sword

---
+++ Assets/PlayerScripts/ThrowSword.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowSword : MonoBehaviour
+{
+    public GameObject gameObject;
+
+    public float swordDamage = 1f;
+    public float swordSpeed=1000f;
+    public float swordMoveTime;
+    public float swordMoveStopTime=2f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        swordMoveTime = 0;
+        GetComponent<Rigidbody>().AddForce(transform.forward*swordSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        swordMoveTime += Time.deltaTime;
+        if( swordMoveTime > swordMoveStopTime)
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;//일정시간이 지나면 그자리에서 멈춤
+        }
+    }
+}
