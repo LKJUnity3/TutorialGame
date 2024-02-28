d473505f389ebca852727e190184b4279490936e
LKJUnity3 <dlrhkdwo2422@gmail.com>
2024-02-28 11:01:21 +0900

    ADD - HitBOX

--- Assets/Script/Boss/HitBoxChange.cs
+++ Assets/Script/Boss/HitBoxChange.cs
@@ -1,11 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.iOS;
 
 public class HitBoxChange : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject CubePrefabs;
     private void Start()
     {
-        
+        for (int i = 0; i < 3; i++)
+        {
+            SpawnCube(i);
+        }
+    }
+
+    private void SpawnCube(int index)
+    {
+        GameObject Clone = Instantiate(CubePrefabs);
+        Renderer renderer = Clone.GetComponent<Renderer>();
+        switch (index)
+        {
+            case 0:
+                Clone.transform.position = new Vector3 (4, 1, 0);
+                renderer.material.color = Color.red;
+                break;
+            case 1:
+                Clone.transform.position = new Vector3 (-4,1,0);
+                renderer.material.color = Color.green;
+                break;
+            case 2:
+                Clone.transform.position = new Vector3(0, 1, 4);
+                renderer.material.color = Color.blue;
+                break;
+        }
     }
 }
