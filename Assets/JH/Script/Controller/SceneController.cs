6581fb2dec5e7593cb3bf4fc835f984184ca2390
shahon922 <shahon922@naver.com>
2024-02-28 20:00:15 +0900

    FIXDE - 씬 이동, CHORE - 맵 스카이박스 색 조정
    맵을 이동하면 색이 다른 색으로 나오는 상황
    해결 - Lighting에서 제너레이터해서 파일로 만들어 색을 저장/불러오기하도록 만듬

---
+++ Assets/JH/Script/Controller/SceneController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    Scene SceneName;
+
+    // bool값으로 나 입장했었다 값을 가져 온 뒤
+    // position값 수정
+    public static bool entering = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneName = SceneManager.GetActiveScene();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(SceneName.name == "MainScene")
+        {
+            SceneManager.LoadScene("JHScene");
+        }
+        else if(SceneName.name == "JHScene")
+        {
+            entering = true;
+            SceneManager.LoadScene("MainScene");
+        }
+    }
+
+    public void GameStartBtn()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+}
---
+++ Assets/JH/Script/Manager/GameManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public GameObject Player;
+
+    public int[] colorList = new int[3];
+
+  
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
