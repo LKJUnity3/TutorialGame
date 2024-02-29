851a297c31439de66ba52678013ebc9baaa2511d
shahon922 <shahon922@naver.com>
2024-02-29 19:16:55 +0900

    ADD - 게임매니저 내용 추가
    게임 오버, 클리어 내용 추가
    시간 관리 추가

--- Assets/JH/Script/Manager/GameManager.cs
+++ Assets/JH/Script/Manager/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,11 +8,15 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
-    public GameObject Player;
+    public GameObject gameOver;
+    public GameObject gameClear;
+    public TMP_Text timeNumTxt;
+
+    float sec;
+    int min;
 
     public int[] colorList = new int[3];
 
-  
 
     private void Awake()
     {
@@ -30,13 +35,36 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Timer();
     }
 
+    private void Timer()
+    {
+        sec += Time.deltaTime;
+   
+        if ((int)sec > 59)
+        {
+            sec = 0;
+            min++;
+        }
+    }
+
+   public void GameOver()
+   {
+        gameOver.SetActive(true);
+        Time.timeScale = 0;
+   }
+
+    public void GameClear()
+    {
+        timeNumTxt.text = string.Format("{0:D2}:{1:D2}", min, (int)sec);
+        gameClear.SetActive(true);       
+        Time.timeScale = 0;
+    }
 }
