15afb8c2732939e6d705c6f11201f64c0e14d13a
LKJUnity3 <dlrhkdwo2422@gmail.com>
2024-02-29 10:25:34 +0900

    ADD - dotori

--- Assets/Script/Boss2/SpawnDotori.cs
+++ Assets/Script/Boss2/SpawnDotori.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpawnDotori : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject dotoriPrefab;
+    private bool isNull = false;
+    private float time = 5f;
+    [SerializeField]
+    private Transform Player;
+
+    private void Start()
     {
-        
+        DotoriSpawn();
+    }
+    private void Update()
+    {
+        if (isNull)
+        {
+            time -= Time.deltaTime;
+            if (time < 0)
+            {
+                isNull = false;
+                time = 5f;
+                DotoriSpawn();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DotoriSpawn()
     {
-        
+        GameObject Clone = Instantiate(dotoriPrefab);
+        Clone.transform.position = transform.position;
+        Clone.GetComponent<DotoriMove>().Setup(Player,this);
+        Clone.GetComponent<Renderer>().material.color = Color.black;
+    }
+
+    public void DestroyDotori(GameObject gameObject)
+    {
+        Destroy(gameObject);
+        isNull = true;
     }
 }
